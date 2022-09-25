145ae11f76a4e394b6c6717fce5b6451d15d361f
kennethpdang <kennethpdang@gmail.com>
2022-09-25 15:03:30 -0500

    Added navigation bar component.

---
+++ components/Navigation_bar.js
@@ -0,0 +1,9 @@
+// File Description: This file is for your navigation bar.
+import { render } from "react-dom";
+
+// Packages to Import:
+
+// Main Function Component:
+function NavigationBar() {
+    render()
+}
